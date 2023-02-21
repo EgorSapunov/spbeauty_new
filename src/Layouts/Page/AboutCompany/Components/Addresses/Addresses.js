4dcbc116f1f12facf7043eeb8fd40e0dcbe86db0
EgorSapunov <123659649+EgorSapunov@users.noreply.github.com>
2023-02-21 20:47:25 +0000

    стиль txt profile + list element + adresses

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -5,7 +5,7 @@ import ListElement from "./ListElement";
 
 const ListContainer = styled.div`
     outline: none;
-    botder: 1px;
+    border: 1px solid #E2E8F0;
     border-radius: 8px;
     background-color: #fff;
     opacity: 100%
@@ -13,13 +13,15 @@ const ListContainer = styled.div`
 
 function Addresses() {
     return (
-        <ListContainer className='d-inline-flex flex-column gap-4 p-4 mt-4 col'>
+        <ListContainer className='d-inline-flex flex-column gap-4 p-4 col justify-content-between'>
+            <div className='d-inline-flex flex-column'>
             <div className='title'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column pt-2 pb-2 gap-2'>
+            <div className='d-inline-flex flex-column pt-2 pb-2 gap-2 mt-4'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <SecondaryButton>Добавить филиал</SecondaryButton>
             </div>
+            </div>
+            <SecondaryButton>Добавить филиал</SecondaryButton>
         </ListContainer>
     );
 }
